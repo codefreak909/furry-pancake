af493f043a6cfeb36526ea14ad0c1b9d1f6b590a
codefreak909 <crazyapachedown@gmail.com>
2019-01-29 16:43:02 +0530

    Database and parser fixed

--- index.php
+++ index.php
@@ -11,57 +11,57 @@
 		function myFunction(e) {
     		//cument.getElementById("myText").value = e.currentTarget.id;
     		var xmlhttp = new XMLHttpRequest();
-    		if (e.currentTarget.id == "country") {
+    		if (e.currentTarget.id == "maker") {
 	        	xmlhttp.onreadystatechange = function() {
 		            if (this.readyState == 4 && this.status == 200) {
-				        	document.getElementById("state").innerHTML = this.responseText;
+				        	document.getElementById("model").innerHTML = this.responseText;
 		            }
 		        };
-		    } else if (e.currentTarget.id == "state") {
+		    } else if (e.currentTarget.id == "model") {
 		    	xmlhttp.onreadystatechange = function() {
 		            if (this.readyState == 4 && this.status == 200) {
-				        	document.getElementById("city").innerHTML = this.responseText;
+				        	document.getElementById("part").innerHTML = this.responseText;
 		            }
 		        };
-		    } else if (e.currentTarget.id == "city")  {
+		    } else if (e.currentTarget.id == "part")  {
 		    	xmlhttp.onreadystatechange = function() {
 		            if (this.readyState == 4 && this.status == 200) {
-				        	document.getElementById("pincode").innerHTML = this.responseText;
+				        	document.getElementById("year").innerHTML = this.responseText;
 		            }
 		        };
 		    }
-	        if (e.currentTarget.id == "country")
-	        	xmlhttp.open("GET", "show.php?country=" + e.target.value, true);
-	        else if (e.currentTarget.id == "state") 
-	        	xmlhttp.open("GET", "show.php?state=" + e.target.value, true);
-	        else if (e.currentTarget.id == "city") 
-	        	xmlhttp.open("GET", "show.php?city=" + e.target.value, true);
+	        if (e.currentTarget.id == "maker")
+	        	xmlhttp.open("GET", "show.php?maker=" + e.target.value, true);
+	        else if (e.currentTarget.id == "model") 
+	        	xmlhttp.open("GET", "show.php?model=" + e.target.value, true);
+	        else if (e.currentTarget.id == "part") 
+	        	xmlhttp.open("GET", "show.php?part=" + e.target.value, true);
 	        xmlhttp.send();
 		}
 	</script>
 </head>
 <body>
 	<form>
-		<select name="" id="country" oninput="myFunction(event)">
-			<option disabled selected>Country</option>
+		<select name="" id="maker" oninput="myFunction(event)">
+			<option disabled selected>Maker</option>
 			<?php
-				$conn = new mysqli('localhost','dude','wowdudedb','test');
+				$conn = new mysqli('localhost','dude','wowdudedb','project_auto');
 
-				$sql="SELECT country_name FROM country";
+				$sql="SELECT maker_name FROM tbl_car_maker";
 				$result=$conn->query($sql);
 				while ($row=$result->fetch_assoc()) {
-					echo "<option value=\"".$row['country_name']."\">".$row['country_name']."</option>";
+					echo "<option value=\"".$row['maker_name']."\">".$row['maker_name']."</option>";
 				}
 			?>
 		</select> <br>
-		<select name="" id="state" oninput="myFunction(event)">
-			<option disabled selected>State</option>
+		<select name="" id="model" oninput="myFunction(event)">
+			<option disabled selected>Model</option>
 		</select><br>
-		<select name="" id="city" oninput="myFunction(event)">
-			<option disabled selected>City</option>
+		<select name="" id="part" oninput="myFunction(event)">
+			<option disabled selected>Parts</option>
 		</select><br>
-		<select name="" id="pincode" oninput="myFunction(event)">
-			<option disabled selected>Pincode</option>
+		<select name="" id="year" oninput="myFunction(event)">
+			<option disabled selected>Year</option>
 		<select><br>
 	</form>
 </body>
--- parsecsv.php
+++ parsecsv.php
@@ -1,15 +1,50 @@
 <?php
 	$conn =  new mysqli('localhost','dude','wowdudedb','project_auto');
+
+	function ifExists($conn, $node, $item) {
+		$sql = "SELECT ".$node."_name FROM tbl_car_".$node." WHERE ".$node."_name=\"".$item."\"";
+		#echo $sql;
+		$result = $conn->query($sql);
+		if($result) {
+			if (mysqli_num_rows($result)>0) {
+				#echo "wow";
+				return 1;
+			}
+			else {
+				#echo "nowow";
+				return 0;
+			}
+		}
+	}
+
+	function extractId($conn, $node, $item) {
+		$sql = "SELECT ".$node."_id FROM tbl_car_".$node." WHERE ".$node."_name=\"".$item."\"";
+		#echo $sql;
+		$result = $conn->query($sql);
+		if($result) {
+			if (mysqli_num_rows($result)>0) {
+				$row = $result->fetch_assoc();
+				#echo "wow";
+				return $row[$node.'_id'];
+			}
+			else {
+				#echo "nowow";
+				return 0;
+			}
+		}
+	}
+
 	$file = fopen("zaxon.csv","r");
 	$maker = "";
 	$model = "";
 	$part = "";
 	$data = fgetcsv($file);
-	print_r($data);
+	#print_r($data);
 	$n = 0;
 
-	while($data = fgetcsv($file)) { 
-		#$data = fgetcsv($file);
+	#while($data = fgetcsv($file))
+	for($i=0;$i<500;$i++) { 
+		$data = fgetcsv($file);
 		#print_r($data);
 		$data[4] = str_replace('Used ', '', $data[4]);
 		$data[4] = str_replace(' Parts', '', $data[4]);
@@ -19,30 +54,33 @@
 		$data[8] = str_replace(' Part', '', $data[8]);
 		#print_r($data);
 		#echo "$maker ".$data[4]."/n";
-		if ($maker!=$data[4]) {
+		if (!ifExists($conn,'maker',$data[4])) {
 			$sql = "INSERT INTO tbl_car_maker(maker_name) VALUES(\"".$data[4]."\")";
 			$result = $conn->query($sql);
 		}
-		$sql = "SELECT maker_id FROM tbl_car_maker WHERE maker_name=\"".$data[4]."\"";
-		$result = $conn->query($sql);
-		$row=$result->fetch_assoc();
-		$mid=$row['maker_id'];
-		if($model!=$data[6]) {
-			$sql = "INSERT INTO tbl_car_model(model_name,maker_id) VALUES(\"".$data[6]."\",\"".$mid."\")";
+		#$sql = "SELECT maker_id FROM tbl_car_maker WHERE maker_name=\"".$data[4]."\"";
+		#$result = $conn->query($sql);
+		#$row=$result->fetch_assoc();
+		#$mid=$row['maker_id'];
+		if(!ifExists($conn,'model',$data[6])) {
+			$sql = "INSERT INTO tbl_car_model(model_name) VALUES(\"".$data[6]."\")";
 			$result = $conn->query($sql);
 		}
-		$sql = "SELECT model_id FROM tbl_car_model WHERE model_name=\"".$data[6]."\"";
+		#$sql = "SELECT model_id FROM tbl_car_model WHERE model_name=\"".$data[6]."\"";
+		#$result = $conn->query($sql);
+		#$row=$result->fetch_assoc();
+		#$moid=$row['model_id'];
+		if(!ifExists($conn,'part',$data[8])) {
+			$sql = "INSERT INTO tbl_car_part(part_name) VALUES(\"".$data[8]."\")";
+			$result = $conn->query($sql);
+		}
+		#echo extractId($conn,'part',$data[8]);
+		$sql = "INSERT INTO tbl_inventory(part_id,model_id) VALUES(\"".extractId($conn,'part',$data[8])."\",\"".extractId($conn,'model',$data[6])."\")";
 		$result = $conn->query($sql);
-		$row=$result->fetch_assoc();
-		$moid=$row['model_id'];
 
-		$sql = "INSERT INTO tbl_car_part(part_name,model_id) VALUES(\"".$data[8]."\",\"".$moid."\")";
-		#echo "$sql";
-		$result = $conn->query($sql);
-
-		$maker= $data[4];
-		$model= $data[6];
-		$part= $data[8];
+		#$maker= $data[4];
+		#$model= $data[6];
+		#$part= $data[8];
 		$n++;
 		echo "$n\n";
 	}
