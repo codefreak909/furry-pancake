f7a38c5bade243b7342096d7361c6331612b79d8
codefreak909 <crazyapachedown@gmail.com>
2019-01-29 12:50:08 +0530

    Tables created

---
+++ data_autoparts.sql
@@ -0,0 +1,63 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `tbl_car_maker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_car_maker` (
+  `maker_id` int(5) NOT NULL AUTO_INCREMENT,
+  `maker_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`maker_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40000 ALTER TABLE `tbl_car_maker` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_car_maker` ENABLE KEYS */;
+DROP TABLE IF EXISTS `tbl_car_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_car_model` (
+  `model_id` int(5) NOT NULL AUTO_INCREMENT,
+  `model_name` varchar(100) NOT NULL,
+  `maker_id` int(5) DEFAULT NULL,
+  PRIMARY KEY (`model_id`),
+  KEY `maker_id` (`maker_id`),
+  CONSTRAINT `tbl_car_model_ibfk_1` FOREIGN KEY (`maker_id`) REFERENCES `tbl_car_maker` (`maker_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40000 ALTER TABLE `tbl_car_model` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_car_model` ENABLE KEYS */;
+DROP TABLE IF EXISTS `tbl_car_part`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_car_part` (
+  `part_id` int(5) NOT NULL AUTO_INCREMENT,
+  `part_name` varchar(100) NOT NULL,
+  `model_id` int(5) DEFAULT NULL,
+  PRIMARY KEY (`part_id`),
+  KEY `model_id` (`model_id`),
+  CONSTRAINT `tbl_car_part_ibfk_1` FOREIGN KEY (`model_id`) REFERENCES `tbl_car_model` (`model_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40000 ALTER TABLE `tbl_car_part` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_car_part` ENABLE KEYS */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
